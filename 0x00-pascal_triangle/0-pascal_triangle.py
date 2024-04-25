14a8501f7c1ec241358d29f7520c4c1ffcff2342
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-04-25 13:20:18 +0100

    update

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-"""runing pascal"""
+"""runing pascal triangle"""
 
 
 def pascal_triangle(n):
     """run pascal"""
-    if n <= 0:
-        return []
-
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)
-        triangle.append(row)
-
-    return triangle
+    array = []
+    for i in range(n):
+        array.append(list(str(11**i)))
+    return array
