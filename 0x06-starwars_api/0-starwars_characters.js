6a673f8ad6c3fc5e5784b55dd0d00eb8d8f5cc9d
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-06-06 14:01:44 +0100

    semistandard check

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -12,49 +12,49 @@ const characterNames = []; // Names of the characters
 
 // Fetch the list of character URLs from the movie endpoint
 const fetchCharacterUrls = async () => {
-    await new Promise(resolve => request(movieApiUrl, (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-            console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-        } else {
-            const movieData = JSON.parse(body);
-            characterUrls = movieData.characters;
-            resolve();
-        }
-    }));
+  await new Promise(resolve => request(movieApiUrl, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+    } else {
+      const movieData = JSON.parse(body);
+      characterUrls = movieData.characters;
+      resolve();
+    }
+  }));
 };
 
 // Fetch the names of characters from the character URLs
 const fetchCharacterNames = async () => {
-    if (characterUrls.length > 0) {
-        for (const url of characterUrls) {
-            await new Promise(resolve => request(url, (err, res, body) => {
-                if (err || res.statusCode !== 200) {
-                    console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-                } else {
-                    const characterData = JSON.parse(body);
-                    characterNames.push(characterData.name);
-                    resolve();
-                }
-            }));
+  if (characterUrls.length > 0) {
+    for (const url of characterUrls) {
+      await new Promise(resolve => request(url, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+        } else {
+          const characterData = JSON.parse(body);
+          characterNames.push(characterData.name);
+          resolve();
         }
-    } else {
-        console.error('Error: No characters found for the movie');
+      }));
     }
+  } else {
+    console.error('Error: No characters found for the movie');
+  }
 };
 
 // Main function to retrieve and print character names
 const printCharacterNames = async () => {
-    await fetchCharacterUrls();
-    await fetchCharacterNames();
+  await fetchCharacterUrls();
+  await fetchCharacterNames();
 
-    // Print each character name on a new line
-    for (const name of characterNames) {
-        if (name === characterNames[characterNames.length - 1]) {
-            process.stdout.write(name);
-        } else {
-            process.stdout.write(name + '\n');
-        }
+  // Print each character name on a new line
+  for (const name of characterNames) {
+    if (name === characterNames[characterNames.length - 1]) {
+      process.stdout.write(name);
+    } else {
+      process.stdout.write(name + '\n');
     }
+  }
 };
 
 printCharacterNames();
