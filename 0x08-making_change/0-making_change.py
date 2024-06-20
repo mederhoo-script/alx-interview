c8b58ad5cce2c27510a5fbddd9a120f67f49c637
Hammed Amusat Oriyomi <mederhoo@gmail.com>
2024-06-20 23:23:20 +0100

    update

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,36 +8,29 @@ import sys
 
 def makeChange(coins, total):
     '''
-    Determine the minimum number of coins needed to meet a given amount total.
-    If total is 0 or less, return 0. If total cannot be met by any number of coins, return -1.
-
-    Parameters:
-    coins (list): A list of integers representing the values of available coins.
-    total (int): The target amount to achieve using the fewest number of coins.
-
-    Returns:
-    int: The minimum number of coins needed to make up the total, or -1 if it's not possible.
+    Return: fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
     '''
     if total <= 0:
         return 0
+    table = [sys.maxsize for i in range(total + 1)]
+    table[0] = 0
+    m = len(coins)
+    for i in range(1, total + 1):
+        for j in range(m):
+            if coins[j] <= i:
+                subres = table[i - coins[j]]
+                if subres != sys.maxsize and subres + 1 < table[i]:
+                    table[i] = subres + 1
 
-    # Create a list to store the minimum number of coins for each amount from 0 to total
-    dp = [sys.maxsize] * (total + 1)
-    dp[0] = 0  # Zero coins are needed to make the amount zero
-
-    # Iterate over each coin in the given list
-    for coin in coins:
-        # For each coin, update the dp array for all amounts that can be reached with this coin
-        for amount in range(coin, total + 1):
-            if dp[amount - coin] != sys.maxsize:
-                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    # If dp[total] is still sys.maxsize, it means the total cannot be made up with the given coins
-    return dp[total] if dp[total] != sys.maxsize else -1
+    if table[total] == sys.maxsize:
+        return -1
+    return table[total]
 
 
 # Main function for testing the makeChange function
 if __name__ == "__main__":
     # Example test cases
-    print(makeChange([1, 2, 25], 37))  # Expected output: 7
-    print(makeChange([1256, 54, 48, 16, 102], 1453))  # Expected output: -1
+    print(makeChange([1, 2, 25], 37))
+    print(makeChange([1256, 54, 48, 16, 102], 1453))
